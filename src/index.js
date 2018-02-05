5b3afa16e626b1eafbf42f5b7dda5fa02ee5e002
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-02-04 21:01:55 -0300

    🔧  configure eslint

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// eslint-disable-next-line
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
