09503ef15519bc2fa1dc193ff52ef7778ff6ae57
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-03-31 19:05:22 -0300

    ✨ scroll to top whenever route changes

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import Header from '../../containers/Layout/Header';
 import Footer from './Footer';
 import Spaced from './Spaced';
+import { locationShape } from '../../shapes';
 import { colors } from '../../theme';
 
 const styles = {
@@ -18,20 +20,35 @@ const styles = {
   },
 };
 
-function AILayout({ children }) {
-  return (
-    <div style={styles.background}>
-      <Header />
-      <Spaced style={styles.body}>
-        {children}
-      </Spaced>
-      <Footer />
-    </div>
-  );
+class AILayout extends Component {
+  static propTypes = {
+    location: locationShape.isRequired,
+    children: PropTypes.node.isRequired,
+  };
+
+  componentWillReceiveProps({ location: { pathname, search } }) {
+    const pathnameChanged = pathname !== this.props.location.pathname;
+    const searchChanged = search !== this.props.location.search;
+
+    if (this.contentTop && (pathnameChanged || searchChanged)) {
+      this.contentTop.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div style={styles.background}>
+        <Header />
+        <div ref={(el) => { this.contentTop = el; }} />
+        <Spaced style={styles.body}>
+          {children}
+        </Spaced>
+        <Footer />
+      </div>
+    );
+  }
 }
 
-AILayout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-export default AILayout;
+export default withRouter(AILayout);
