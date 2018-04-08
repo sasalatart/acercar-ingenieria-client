c70d300264012e97b8bbc3f94ddc0668eb259872
Sebastián Salata R-T <sa.salatart@gmail.com>
2018-04-08 15:52:10 -0300

    ✨ redirect to nested article within major route when article has a majorId but route does not

--- src/containers/Articles/Article/index.jsx
+++ src/containers/Articles/Article/index.jsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter, Redirect } from 'react-router-dom';
 import { injectIntl } from 'react-intl';
 import {
   collection,
@@ -8,6 +10,25 @@ import {
 import { getIsFetching } from '../../../store/ducks/loading';
 import WithAuthorization from '../../../hoc/WithAuthorization';
 import Article from '../../../components/Articles/Article';
+import ROUTES from '../../../routes';
+import { articleShape, matchShape } from '../../../shapes';
+
+function ArticleWrapper(props) {
+  const { article, match: { params } } = props;
+
+  return article && article.majorId && !params.majorId
+    ? <Redirect to={ROUTES.ARTICLE(article.id, article.majorId)} />
+    : <Article {...props} />;
+}
+
+ArticleWrapper.propTypes = {
+  article: articleShape,
+  match: matchShape.isRequired,
+};
+
+ArticleWrapper.defaultProps = {
+  article: undefined,
+};
 
 function mapStateToProps(state, ownProps) {
   const params = { ...ownProps.match.params, collection };
@@ -27,5 +48,5 @@ function mapDispatchToProps(dispatch, ownProps) {
   };
 }
 
-const connectedComponent = connect(mapStateToProps, mapDispatchToProps)(Article);
-export default injectIntl(WithAuthorization(connectedComponent));
+const connectedComponent = connect(mapStateToProps, mapDispatchToProps)(ArticleWrapper);
+export default injectIntl(withRouter(WithAuthorization(connectedComponent)));
